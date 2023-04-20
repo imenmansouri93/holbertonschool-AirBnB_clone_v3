08a4c515b6d435fd4a21a6334f0b70a4447cfaac
imenmansouri93 <imennmansourii@gmail.com>
2023-04-20 14:31:50 +0100

    new

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,8 +12,6 @@ def teardown(exception):
     storage.close()
 
 
-
-
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default='5000')
