f55ac88afd224f210f4f695cb81acc95757ce3e4
imenmansouri93 <imennmansourii@gmail.com>
2023-04-20 23:03:37 +0100

    new

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+
+"""
 
 from flask import Blueprint
 from api.v1.views import states
+from api.v1.views import cities
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
